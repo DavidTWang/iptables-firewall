ba25fbd6adc1c9456f43e352506e5b8946135c58
Hakus <altenotiz@gmail.com>
2015-02-07 15:23:38 -0800

    Initial commit

---
+++ assignment2.py
@@ -0,0 +1,16 @@
+import os
+
+def run_script(option):
+	return {
+		'1': "firewall.sh",
+		'2': "internal.sh",
+		'3': "reset.sh",
+	}.get(option, "invalid")
+
+print "COMP8006 Assignment 2"
+print "1 - Firewall computer setup"
+print "2 - Internal computer setup"
+print "3 - Reset to default"
+option = raw_input("Option: ")
+os.system("./" + run_script(option))
+print "Finished"
