c027d9a6ba83540603c07caed129a9b74c7a4b6e
Hakus <altenotiz@gmail.com>
2015-02-07 21:54:35 -0800

    temp commit

--- assignment2.py
+++ assignment2.py
@@ -9,25 +9,26 @@ INTERNAL_IP = "192.168.10.2"
 SUBNET_ADDR = "192.168.10.0/24"
 
 
-def reset_iptables():
-	os.system("clear; iptables -F; iptables -X; iptables -L")
+def reset():
+	os.system("clear; iptables -F; iptables -X")
+	os.system("iptables -P INPUT ACCEPT; iptables -P OUTPUT ACCEPT; iptables -P FORWARD ACCEPT")
+	print "Firewall setup reset"
 
 def setup_system(host_type):
 	if(host_type == "firewall"):
 		os.system("ifconfig %s %s up" 
 			% (PRIVATE_INTERFACE, FIREWALL_INTERFACE_IP))
 		os.system("echo \"1\" > /proc/sys/net/ipv4/ip_forward")
-		os.system("route add -net 192.168.0.0 network 255.255.255.0 gw %s" 
-			% FIREWALL_IP)
-		os.system("route add -net %s gw %s" 
-			% (SUBNET_ADDR, FIREWALL_INTERFACE_IP))
-		os.system("iptables -t nat -A POSTROUTING -s 192.168.10.0 -o %s -j SNAT --to-sort %s" 
-			% (PUBLIC_INTERFACE, FIREWALL_IP))
-		os.system("iptables -t nat -A PREROUTING -i %s -j DNAT --to-destination %s"
-			% (PUBLIC_INTERFACE, INTERNAL_IP))
-		os.system("iptables -t nat -A POSTROUTING -o %s -j MASQUERADE" 
+		os.system("route add -net 192.168.0.0 netmask 255.255.255.0 gw %s" % FIREWALL_IP)
+		os.system("route add -net %s gw %s" % (SUBNET_ADDR, FIREWALL_INTERFACE_IP))
+		# os.system("iptables -t nat -A POSTROUTING -s 192.168.10.0 -o %s -j SNAT --to-source %s" 
+		# 	% (PUBLIC_INTERFACE, FIREWALL_IP))
+		# os.system("iptables -t nat -A PREROUTING -i %s -j DNAT --to-destination %s"
+		# 	% (PUBLIC_INTERFACE, INTERNAL_IP))
+		os.system("iptables -t nat -A POSTROUTING -o %s -j MASQUERADE"
 			% PUBLIC_INTERFACE)
 		print "Finished setting up firewall host"
+
 	elif(host_type == "internal"):
 		os.system("ifconfig %s down" % PUBLIC_INTERFACE)
 		os.system("ifconfig %s %s up" % (PRIVATE_INTERFACE, INTERNAL_IP))
@@ -35,7 +36,25 @@ def setup_system(host_type):
 		print "Finished setting up internal host. Don't forget to set nameservers."
 
 def execute_firewall():
-	pass
+	# Set all default policies to DROP
+	os.system("iptables -P INPUT DROP; iptables -P OUTPUT DROP; iptables -P FORWARD DROP")
+
+	# Drop all the packets with source ip matching the internal network
+	os.system("iptables -A FORWARD -i em1 -p tcp -s 192.168.10.0/24 -j DROP")
+
+	# Block all external traffic directed to ports 32768 - 32775, 137 - 139, TCP prots 111 and 515
+	os.system("iptables -A FORWARD -p tcp -m multiport --dports 111,515,32768:32775 -j DROP")
+
+	# Drop telnet connections
+	os.system("iptables -A FORWARD -p tcp --sport 23 -j DROP")
+	os.system("iptables -A FORWARD -p tcp --dport 23 -j DROP")
+
+	# For FTP and SSH services, set control connections to "Minimum Delay" and FTP data to "Maximum Throughput"
+	os.system("iptables -A PREROUTING -t mangle -p tcp --sport ssh -j TOS --set-tos Minimize-Delay")
+	os.system("iptables -A PREROUTING -t mangle -p tcp --sport ftp -j TOS --set-tos Minimize-Delay")
+	os.system("iptables -A PREROUTING -t mangle -p tcp --sport ftp-data -j TOS --set-tos Maximize-Throughput")
+	
+	print "Firewall activated"
 
 def run_script(options):
 	for option in options:
@@ -44,7 +63,7 @@ def run_script(options):
 		elif(option == '2'):
 			setup_system("internal")
 		elif(option == '3'):
-			reset_iptables()
+			reset()
 		elif(option == '4'):
 			execute_firewall()
 		elif(option == '0'):
